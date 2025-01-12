188feb5ad42b07e87f7957a15c4f616749eb1511
Sonu Kumar <121798722+mahi720@users.noreply.github.com>
2025-01-12 15:47:17 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const Banner = () => {
+  const { token } = useContext(AppContext);
   const navigate = useNavigate();
   return (
     <div className="flex bg-primary rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10">
@@ -13,16 +16,26 @@ const Banner = () => {
           <p>Book Appointment</p>
           <p className="mt-4">With 100+ Trusted Doctors</p>
         </div>
-        <button
-          onClick={() => {
-            navigate("/login");
-            scrollTo(0, 0);
-          }}
-          className="bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all"
-        >
-          Create Account
-        </button>
-      </div>
+        {token ? (
+          <button
+            onClick={() => {
+              navigate("/about");
+            }}
+            className="bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all"
+          >
+            Explore
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              navigate("/login");
+              scrollTo(0, 0);
+            }}
+            className="bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all"
+          >
+            Create Account
+          </button>
+        )}
 
       {/* -----------right side----------- */}
       <div className="hidden md:block md:w-1/2 lg:w-[370px] relative">
