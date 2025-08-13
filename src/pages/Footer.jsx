87aa0c8b4f655f68b51b3f3c9ef11c9a898b66f3
Sonu Kumar <121798722+mahi720@users.noreply.github.com>
2025-08-13 22:37:18 +0530

    Update Footer.jsx
    change the email and contect number

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -34,8 +34,8 @@ const Footer = () => {
         <div>
           <p className="text-xl font-medium mb-5">Get In Touch</p>
           <ul className="flex flex-col gap-2 text-gray-600">
-            <li>+1-222-343-3123</li>
-            <li>crowdlion@gmail.com</li>
+            <li>+91-000-000-0000</li>
+            <li>somammahi@gmail.com</li>
           </ul>
         </div>
       </div>
